6f030246828fe7eeef19de7f2893b134fd8d8975
Amin Ghadimian <amin@gmail.com>
2020-07-22 19:53:38 +0430

    Updating git ignore

--- build/classes/com/amin/technom/config/initial_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `technom_db`.`users` (`full_name`,`username`, `email`, `enabled`, `password`) VALUES ('مدیر','admin', 'aa@ff.com', TRUE, '$2y$12$2FsYh15iTt07gz1IKz4YqetSWwRpIjP.iKqQKfKvmqwlfKpLmx9Q.');	
-INSERT INTO `technom_db`.`authorities` (`authority`, `username`) VALUES ('ROLE_ADMIN', 'admin');	
